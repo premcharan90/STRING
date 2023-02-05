83accce5bab97d12e4bd73978193f9776893768d
PremCharan <124121821+premcharan90@users.noreply.github.com>
2023-02-05 23:23:54 +0530

    Length

--- LastIndex.java
+++ LastIndex.java
@@ -1,6 +1,6 @@
 public class LastIndex{
     public static void main(String[] args){
-        String s1="this is index of example";//there are 2 's' characters in this sentence
-        int index1=s1.lastIndexOf('s');//returns last index of 's' char value
+        String s1="this is index of example";
+        int index1=s1.lastIndexOf('s');
         System.out.println(index1);//6
     }}
